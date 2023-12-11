f4fdc3a5565631bab2ba521d9ba5068de9730b30
anastasiiaipe <143907938+anastasiiaipe@users.noreply.github.com>
2023-12-11 20:12:47 +0100

    js update

---
+++ js/task-1.js
@@ -0,0 +1,31 @@
+"use strict";
+// const first = { propA: 5, propB: 10, propC: 50 };
+// const second = { propC: 15 };
+
+// const third = { propB: 20, ...first, ...second };
+// console.log(third); // { propA: 5, propB: 10, propC: 15 }
+
+// const fourth = { ...first, ...second, propB: 20 };
+// console.log(fourth); // { propA: 5, propB: 20, propC: 15 }
+
+// const fifth = { ...first, propB: 20, ...second };
+// console.log(fifth); // { propA: 5, propB: 20, propC: 15 }
+
+// const objA = {
+//   x: 1,
+//   y: 2,
+// };
+
+// const objB = {
+//   y: 3,
+// };
+
+// const result = {
+//   x: 5,
+//   ...objA,
+//   y: 10,
+//   ...objB,
+//   z: 15,
+// };
+
+// console.log(result);
